285f9ad63d37de109e8c0b3e11faa44be3967a52
Isac Ribeiro <isacribeiro@sandbeach.com.br>
2024-07-23 16:09:03 -0300

    primeiro commit

---
+++ Calculadora.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal static class Calculadora
+    {
+        private static double num1, num2;
+
+        public static void Somar()
+        {
+            num1 = 0;
+            num2 = 0;
+            Console.WriteLine("Digite um número");
+            num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite outro número:");
+            num2 = double.Parse(Console.ReadLine());
+
+            double result = num1 + num2;
+            Console.WriteLine("O resultado é: " + result);
+        }
+
+        public static void Subtrair()
+        {
+            num1 = 0;
+            num2 = 0;
+            Console.WriteLine("Digite um número");
+            num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite outro número:");
+            num2 = double.Parse(Console.ReadLine());
+
+            double result = num1 - num2;
+            Console.WriteLine("O resultado é: " + result);
+        }
+
+        public static void Multiplicar()
+        {
+            num1 = 0;
+            num2 = 0;
+            Console.WriteLine("Digite um número");
+            num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite outro número:");
+            num2 = double.Parse(Console.ReadLine());
+
+            double result = num1 * num2;
+            Console.WriteLine("O resultado é: " + result);
+        }
+
+        public static void Dividir()
+        {
+            num1 = 0;
+            num2 = 0;
+            Console.WriteLine("Digite um número");
+            num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite outro número:");
+            num2 = double.Parse(Console.ReadLine());
+
+            if (num2 != 0)
+            {
+                double result = num1 / num2;
+                Console.WriteLine("O resultado é: " + result);
+            }
+
+            else
+            {
+                Console.WriteLine("Não existe divisão por 0");
+            }
+
+        }
+
+        public static void CalcularPotencia()
+        {
+            num1 = 0;
+            num2 = 0;
+            Console.WriteLine("Digite um número para base:");
+            num1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o expoente:");
+            num2 = double.Parse(Console.ReadLine());
+
+            double result = Math.Pow(num1, num2);
+            Console.WriteLine("O resultado é: " + result);
+        }
+
+        public static void CalcularRaizQadrada()
+        {
+            num1 = 0;
+            Console.WriteLine("Digite qual número deseja saber a raíz quadrada:");
+            num1 = double.Parse(Console.ReadLine());
+
+            double result = Math.Sqrt(num1);
+            Console.WriteLine("O resultado é: " + result);
+        }
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Calculadora");
+            Inicio:
+            Console.WriteLine("Digite uma opcão:");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Multiplicação");
+            Console.WriteLine("4 - Divisão");
+            Console.WriteLine("5 - Potenciação");
+            Console.WriteLine("6 - Radiciação\n");
+
+            int opcao = int.Parse(Console.ReadLine());
+
+            if (opcao == 1)
+            {
+                Calculadora.Somar();
+            }
+            else if (opcao == 2)
+            {
+                Calculadora.Subtrair();
+            }
+            else if (opcao == 3)
+            {
+                Calculadora.Multiplicar();
+            }
+            else if (opcao == 4)
+            {
+                 Calculadora.Dividir();
+            }
+            else if (opcao == 5)
+            {
+                Calculadora.CalcularPotencia();
+            }
+            else if (opcao == 6)
+            {
+                Calculadora.CalcularRaizQadrada();
+            }
+
+            else
+            {
+                Console.WriteLine("Opção Invalida!");
+                
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine("Escolha:");
+            Console.WriteLine("1 - Início");
+            Console.WriteLine("2 - Finalizar Programa");
+            opcao = int.Parse(Console.ReadLine());
+
+            if (opcao == 1) {
+                goto Inicio;
+            }
+
+            else
+            {
+                Console.WriteLine("Fim do Programa!");
+                Console.ReadKey();
+            }
+            
+
+        }
+    }
+}
